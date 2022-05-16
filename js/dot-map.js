39f1095c64fbe6730459253c6b2cb21283954871
unknown <belalelmeligy516@gmail.com>
2022-05-16 14:02:43 +0200

    Edit dotmap constraint to not privede more than 8 categories

--- js/dot-map.js
+++ js/dot-map.js
@@ -8,6 +8,9 @@ function validateParams(data) {
     if(getType(data) !== 'Map'){
         throw new Error("You should provide a map");
     }
+    if(data.size > 8) {
+        throw new Error("You should provide no more than 8 categories");
+    }
     for(var val of data.values()){
         if(typeof(val) !== "number") {
             throw new Error("You should provide discrete values")
