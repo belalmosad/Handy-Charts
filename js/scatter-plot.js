0442e1c39ad15d2448d1bfcff2b58c6128ceb6f1
unknown <belalelmeligy516@gmail.com>
2022-05-27 23:32:58 +0200

    Implemented basic dot drawing functionality in scatter plot

--- js/scatter-plot.js
+++ js/scatter-plot.js
@@ -3,9 +3,20 @@ function ScatterPlot(pointsArr) {
 }
 
 ScatterPlot.prototype.draw=function() {
-    let maxValue = getMaxValue(this.pointsArr);
+    let pointsArr = this.pointsArr;
+    let maxValue = getMaxValue(pointsArr);
     let scale = new ScaleGrid(maxValue);
     scale.draw();
+
+    let dotsDiv = createDotsDiv();
+
+
+    pointsArr.forEach(point => {
+        let dot = placeDot(point);
+        dotsDiv.appendChild(dot);
+    });
+
+
 }
 
 
@@ -19,4 +30,43 @@ function getMaxValue(pointsArr) {
         return max;
     });
     return max;
+}
+
+function placeDot(dotCoordinates) {
+    let xPosition = dotCoordinates[0];
+    let yPoistion = dotCoordinates[1];
+
+    let dot = document.createElement('div');
+    dot.classList.add('dot-blue', 'scale-dot');
+
+    // Move vertically
+    var scaleHeight = +getComputedStyle(document.querySelector('.scale')).height.slice(0,-2);
+    var verticalStep = scaleHeight / 10;
+    var topPositionVW = getComputedStyle(dot).top.slice(0,-2) * 100 / window.innerWidth;
+    var verticalStepVW = verticalStep * 100 / window.innerWidth;
+    var newTopPosition = topPositionVW - yPoistion*verticalStepVW;
+    dot.style.setProperty('top', newTopPosition+'vw');
+
+
+    // move horizontally
+    var scaleWidth = +getComputedStyle(document.querySelector('.scale')).width.slice(0,-2);
+    var horizontalStep = scaleWidth / 10;
+    var leftPositionVW = getComputedStyle(dot).left.slice(0,-2) * 100 / window.innerWidth;
+    var horizontalStepVW = horizontalStep * 100 / window.innerWidth;
+    var newLeftPosition = leftPositionVW + xPosition*horizontalStepVW; // change by 30% -> move three horizontal steps
+    dot.style.setProperty('left', newLeftPosition+'vw');
+
+    return dot;
+
+}
+
+
+function createDotsDiv() {
+    let dotsContainerDiv = document.createElement('div');
+    dotsContainerDiv.style.position = 'relative';
+    let dotsDiv = document.createElement('div');
+    dotsDiv.classList.add('dots');
+    dotsContainerDiv.appendChild(dotsDiv);
+    document.body.appendChild(dotsContainerDiv);
+    return dotsDiv;
 }
