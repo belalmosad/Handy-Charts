cd472632f184593ae419659a69ab3d6590099e55
unknown <belalelmeligy516@gmail.com>
2022-06-22 21:07:00 +0200

    edited bar plot draw function so that it returns a reference to the created DOM element (to be reusable in the code 'e.g. appended or deleted')

--- js/bar-plot.js
+++ js/bar-plot.js
@@ -6,9 +6,9 @@ class BarPlot {
         this.dataMaxValue = Math.max(...Array.from(data.values()));
         this.dataMinValue = Math.min(...Array.from(data.values()));
     }
-    setTitle(title) {this.title = title;}
-    setData(data) {this.data = data;}
-    setTheme(theme) {this.theme = theme;}
+    setTitle(title) { this.title = title; }
+    setData(data) { this.data = data; }
+    setTheme(theme) { this.theme = theme; }
     draw() {
 
         let containerDiv = createContainerDiv();
@@ -21,16 +21,16 @@ class BarPlot {
         barsDiv.appendChild(scaleDiv);
         containerDiv.appendChild(barsDiv);
         document.body.appendChild(containerDiv);
-        for(let [key, value] of this.data.entries()) {
+        for (let [key, value] of this.data.entries()) {
             let barDiv = createBar(this.theme);
             barsDiv.appendChild(barDiv);
             setBarHeight(barDiv, value, this.dataMaxValue);
             barDiv.innerHTML = `${key} <br> ${value}`;
-            
+
         }
 
         let allBars = barsDiv.children;
-        for(let bar of allBars) {
+        for (let bar of allBars) {
             bar.addEventListener('mouseover', () => {
                 addBarInfo(bar, this.theme, bar.innerHTML);
             });
@@ -67,11 +67,11 @@ class BarPlot {
             let percent = value / maxValue;
             let elementHeight = barElement.clientHeight;
             let percentToDecrease = 1 - percent;
-            let newElementHeight = elementHeight - percentToDecrease*elementHeight;
-            barElement.style.height = newElementHeight+'px';
-            barElement.style.transform = `translateY(${elementHeight-newElementHeight}px)`;
+            let newElementHeight = elementHeight - percentToDecrease * elementHeight;
+            barElement.style.height = newElementHeight + 'px';
+            barElement.style.transform = `translateY(${elementHeight - newElementHeight}px)`;
         }
-        function addBarInfo(bar,theme, key) {
+        function addBarInfo(bar, theme, key) {
             removeBarInfo();
             let barInfoDiv = document.createElement('div');
             barInfoDiv.classList.add('bar-category', `bar-category-${theme}`);
@@ -80,8 +80,8 @@ class BarPlot {
             positionBarInfo(barInfoDiv, bar);
         }
         function removeBarInfo() {
-            
-            for(let barCategory of document.getElementsByClassName('bar-category')) {
+
+            for (let barCategory of document.getElementsByClassName('bar-category')) {
                 barCategory.parentElement.removeChild(barCategory);
             }
         }
@@ -102,13 +102,15 @@ class BarPlot {
         }
 
         function fillScaleDiv(scaleDiv, min, max) {
-            for(let i = max; i >= 0 ; i -= min) {
+            for (let i = max; i >= 0; i -= min) {
                 let valDiv = document.createElement('div');
                 valDiv.innerHTML = i;
                 scaleDiv.appendChild(valDiv);
             }
         }
-        
+
+        return containerDiv;
+
     }
 }
 
