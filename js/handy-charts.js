6461229ec927cb8ee49269ae6f15f3e5e0fa4fec
unknown <belalelmeligy516@gmail.com>
2022-06-23 12:53:40 +0200

    Added handy-charts.js file to be an entry point and includes all imports for all modules also it contains public functionalities that are shared between all elements

---
+++ js/handy-charts.js
@@ -0,0 +1,7 @@
+import BarPlot from "./bar-plot.js";
+import DotMap from "./dot-map.js";
+import ProgressBar from './progress-bar.js';
+import ScatterPlot from './scatter-plot.js';
+
+
+export {BarPlot, DotMap, ProgressBar, ScatterPlot, deleteElement}
