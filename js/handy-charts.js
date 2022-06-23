596c0925ce7ed612752288bf125c27de319ab608
unknown <belalelmeligy516@gmail.com>
2022-06-23 16:14:38 +0200

    Imported word counter in handy charts file

--- js/handy-charts.js
+++ js/handy-charts.js
@@ -2,9 +2,10 @@ import BarPlot from "./bar-plot.js";
 import DotMap from "./dot-map.js";
 import ProgressBar from './progress-bar.js';
 import ScatterPlot from './scatter-plot.js';
+import WordCounter from "./word-counter.js";
 
 function deleteElement(element) {
     element.htmlDOMElement.parentNode.removeChild(element.htmlDOMElement);
 }
 
-export {BarPlot, DotMap, ProgressBar, ScatterPlot, deleteElement}
+export {BarPlot, DotMap, ProgressBar, ScatterPlot, WordCounter, deleteElement}
