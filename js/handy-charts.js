872d11ee54c822b67dae3f43aa255b93c09260c6
unknown <belalelmeligy516@gmail.com>
2022-06-23 13:11:56 +0200

    Fixed issue with retrieving the HTML DOM element by calling element.htmlDOMElement first

--- js/handy-charts.js
+++ js/handy-charts.js
@@ -3,13 +3,8 @@ import DotMap from "./dot-map.js";
 import ProgressBar from './progress-bar.js';
 import ScatterPlot from './scatter-plot.js';
 
-/**
- * 
- * @param {HTMLElement} element 
- */
-
 function deleteElement(element) {
-    element.parentNode.removeChild(element.htmlDOMElement);
+    element.htmlDOMElement.parentNode.removeChild(element.htmlDOMElement);
 }
 
 export {BarPlot, DotMap, ProgressBar, ScatterPlot, deleteElement}
