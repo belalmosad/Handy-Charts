2811f1103473ef7b6a1ded5a772681a3604e8e3f
unknown <belalelmeligy516@gmail.com>
2022-06-25 23:54:15 +0200

    Added venn-diagram.js for creating VennDiagram Elements

--- js/handy-charts.js
+++ js/handy-charts.js
@@ -3,9 +3,10 @@ import DotMap from "./dot-map.js";
 import ProgressBar from './progress-bar.js';
 import ScatterPlot from './scatter-plot.js';
 import WordCounter from "./word-counter.js";
+import VennDiagram from "./venn-diagram.js";
 
 function deleteElement(element) {
     element.htmlDOMElement.parentNode.removeChild(element.htmlDOMElement);
 }
 
-export {BarPlot, DotMap, ProgressBar, ScatterPlot, WordCounter, deleteElement}
+export {BarPlot, DotMap, ProgressBar, ScatterPlot, WordCounter,VennDiagram, deleteElement}
---
+++ js/venn-diagram.js
@@ -0,0 +1,34 @@
+function VennDiagram(firstSetData, firstSetTheme, secondSetData, secondSetTheme) {
+    this.firstSetData = firstSetData;
+    this.firstSetTheme = firstSetTheme;
+    this.secondSetData = secondSetData;
+    this.secondSetTheme = secondSetTheme;
+}
+
+VennDiagram.prototype.draw = function() {
+    let vennDiagramDiv = document.createElement('div');
+    vennDiagramDiv.classList.add('venn-diagram');
+
+    let firstSetDiv = createFirstSet(this.firstSetTheme);
+    let secondSetDiv = createSecondSet(this.secondSetTheme);
+
+    vennDiagramDiv.appendChild(firstSetDiv);
+    vennDiagramDiv.appendChild(secondSetDiv);
+
+    document.body.appendChild(vennDiagramDiv);
+
+    return vennDiagramDiv;
+
+}
+function createFirstSet(firstSetTheme) {
+    let firstSetDiv = document.createElement('div');
+    firstSetDiv.classList.add('set-one', `set-bg-${firstSetTheme}`);
+    return firstSetDiv;
+}
+function createSecondSet(secondSetTheme) {
+    let secondSetDiv = document.createElement('div');
+    secondSetDiv.classList.add('set-two', `set-bg-${secondSetTheme}`);
+    return secondSetDiv;
+}
+
+export default VennDiagram;
