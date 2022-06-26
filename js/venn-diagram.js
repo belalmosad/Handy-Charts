de6311e0fe390ee965c454e22d90c4991ddb00cf
unknown <belalelmeligy516@gmail.com>
2022-06-26 12:21:48 +0200

    Implemented handling set-one-title and set-two-title functionality in venn-diagram.js

--- js/venn-diagram.js
+++ js/venn-diagram.js
@@ -3,22 +3,24 @@ function VennDiagram(firstSetData, firstSetTheme, secondSetData, secondSetTheme)
     this.firstSetTheme = firstSetTheme;
     this.secondSetData = secondSetData;
     this.secondSetTheme = secondSetTheme;
+    this.firstSetTitle = "Set One";
+    this.secondSetTitle = "Set Two";
 }
 
-VennDiagram.prototype.addFirstSetTitle = function (firstSetTitle = "Set One") {
+VennDiagram.prototype.addFirstSetTitle = function (firstSetTitle) {
     this.firstSetTitle = firstSetTitle;
 }
 
-VennDiagram.prototype.addSecondSetTitle = function (secondSetTitle = "Set Two") {
-    this.secondSettitle = secondSetTitle;
+VennDiagram.prototype.addSecondSetTitle = function (secondSetTitle) {
+    this.secondSetTitle = secondSetTitle;
 }
 
 VennDiagram.prototype.draw = function () {
     let vennDiagramDiv = document.createElement('div');
     vennDiagramDiv.classList.add('venn-diagram');
 
-    let firstSetDiv = createFirstSet(this.firstSetTheme);
-    let secondSetDiv = createSecondSet(this.secondSetTheme);
+    let firstSetDiv = createFirstSet(this.firstSetTitle,this.firstSetTheme);
+    let secondSetDiv = createSecondSet(this.secondSetTitle,this.secondSetTheme);
 
     vennDiagramDiv.appendChild(firstSetDiv);
     vennDiagramDiv.appendChild(secondSetDiv);
@@ -28,14 +30,22 @@ VennDiagram.prototype.draw = function () {
     return vennDiagramDiv;
 
 }
-function createFirstSet(firstSetTheme) {
+function createFirstSet(firstSetTitleContent,firstSetTheme) {
     let firstSetDiv = document.createElement('div');
+    let firstSetTitle = document.createElement('div');
+    firstSetTitle.innerHTML = firstSetTitleContent;
     firstSetDiv.classList.add('set-one', `set-bg-${firstSetTheme}`);
+    firstSetTitle.classList.add('set-one-title', `set-title-${firstSetTheme}`);
+    firstSetDiv.appendChild(firstSetTitle);
     return firstSetDiv;
 }
-function createSecondSet(secondSetTheme) {
+function createSecondSet(secondSetTitleContent,secondSetTheme) {
     let secondSetDiv = document.createElement('div');
+    let secondSetTitle = document.createElement('div');
+    secondSetTitle.innerHTML = secondSetTitleContent;
     secondSetDiv.classList.add('set-two', `set-bg-${secondSetTheme}`);
+    secondSetTitle.classList.add('set-two-title', `set-title-${secondSetTheme}`);
+    secondSetDiv.appendChild(secondSetTitle);
     return secondSetDiv;
 }
 
